d03ec8325957d26d11548311e46c39b2f32de020
Gopal Singh <109897876+GopalSingh09@users.noreply.github.com>
2023-03-16 00:54:23 +0530

    Add files via upload

---
+++ square_using_template_funciton.cpp
@@ -0,0 +1,27 @@
+case 3:{
+q.store();
+break;}
+case 4:{
+cout<<"\nQueue is\n ";
+q.retrieve();
+break;}
+case 5:{
+cout<<"\nExiting...";
+exit(0);
+break;}
+default :{
+cout<<"\nInvalid input\n";
+}}
+cout<<"\nEnter choice: ";
+cin>>ch;
+}while(1);
+getch();
+return 0;
+}
+cout<<"Invalid input\n";
+ break;
+ }
+ }
+ }while(1);
+ return 0;
+}
